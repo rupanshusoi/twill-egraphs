a4628daaa6f8ca2fc7e08c8439f484addd1cdef8
Rupanshu Soi <rupanshusoi@gmail.com>
2026-05-07 16:31:01 -0700

    Update

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ use std::hash::{Hash, Hasher};
 use egg::{Analysis, EGraph, FromOp, Id, Language, Symbol};
 use good_lp::{constraint, default_solver, variable, variables, Expression, SolverModel};
 
-type Rrt = Vec<Vec<i32>>;
+type ResTable = Vec<Vec<i32>>;
+
+pub trait CostModel {
+    fn get_rt(&self, op: &str) -> ResTable;
+}
 
 #[derive(Debug, Clone, Copy)]
 pub struct EdgeData {
@@ -19,19 +23,19 @@ pub struct TileLang {
     pub op: Symbol,
     pub children: Vec<Id>,
     pub edge_attrs: Vec<(i32, i32)>,
-    pub rrt: Rrt,
 }
 
 impl TileLang {
-    pub fn new(op: impl Into<Symbol>, children: Vec<Id>) -> Self {
-        let op = op.into();
-        let n = children.len();
-        let (rrt, edge_attrs) = metadata_for(op.as_str(), n);
-        Self { op, children, edge_attrs, rrt }
+    pub fn new(
+        op: impl Into<Symbol>,
+        children: Vec<Id>,
+        edge_attrs: Vec<(i32, i32)>,
+    ) -> Self {
+        Self { op: op.into(), children, edge_attrs }
     }
 
     pub fn leaf(op: impl Into<Symbol>) -> Self {
-        Self::new(op, vec![])
+        Self::new(op, vec![], vec![])
     }
 
     pub fn edges(&self) -> impl Iterator<Item = EdgeData> + '_ {
@@ -40,44 +44,21 @@ impl TileLang {
             .zip(&self.edge_attrs)
             .map(|(&id, &(d, delta))| EdgeData { id, d, delta })
     }
-
-    /// Number of clock cycles this op occupies (the height of `rrt`).
-    pub fn cost(&self) -> i32 {
-        self.rrt.len() as i32
-    }
-
-    /// Modulo reservation table at the given II: `ii` rows × `n_resources` cols.
-    /// Sums each cycle of the unfolded `rrt` into row (cycle mod ii). Mirrors
-    /// the Python `Node.restable(II)` exactly when each op uses a single
-    /// resource for `cost` consecutive cycles.
-    pub fn restable(&self, ii: usize, n_resources: usize) -> Vec<Vec<i32>> {
-        let mut tbl = vec![vec![0; n_resources]; ii];
-        for (row_idx, row) in self.rrt.iter().enumerate() {
-            let target = row_idx % ii;
-            for (col_idx, &val) in row.iter().enumerate() {
-                if col_idx < n_resources {
-                    tbl[target][col_idx] += val;
-                }
-            }
-        }
-        tbl
-    }
 }
 
-fn metadata_for(op: &str, n_children: usize) -> (Rrt, Vec<(i32, i32)>) {
-    match (op, n_children) {
-        // a/b/c/d toy problem.
-        ("a", 1) => (vec![vec![1, 0]], vec![(0, 2)]),
-        ("b", 2) => (vec![vec![0, 1]], vec![(0, 1), (0, 2)]),
-        ("c", 1) => (vec![vec![1, 0]], vec![(0, 1)]),
-        ("d", 1) => (vec![vec![1, 0], vec![0, 1]], vec![(1, 1)]),
-        // ilp-modulo-sched.py problem. I1=MMA0, I2=SOFTMAX, I3=MMA1.
-        // Edges (e-graph child = producer): I2 <- I1, I3 <- I2, I3 <- I3 (self).
-        ("I1", 0) => (vec![vec![1, 0]; 2], vec![]),
-        ("I2", 1) => (vec![vec![0, 1]; 4], vec![(2, 0)]),
-        ("I3", 2) => (vec![vec![1, 0]; 2], vec![(4, 0), (2, 1)]),
-        _ => (Vec::new(), vec![(0, 0); n_children]),
+/// Modulo reservation table at the given II: `ii` rows × `n_resources` cols.
+/// Sums each cycle of the unfolded `rt` into row (cycle mod ii).
+fn modulo_rt(rt: &ResTable, ii: usize, n_resources: usize) -> Vec<Vec<i32>> {
+    let mut tbl = vec![vec![0; n_resources]; ii];
+    for (row_idx, row) in rt.iter().enumerate() {
+        let target = row_idx % ii;
+        for (col_idx, &val) in row.iter().enumerate() {
+            if col_idx < n_resources {
+                tbl[target][col_idx] += val;
+            }
+        }
     }
+    tbl
 }
 
 impl PartialEq for TileLang {
@@ -128,7 +109,8 @@ impl fmt::Display for TileLang {
 impl FromOp for TileLang {
     type Error = std::convert::Infallible;
     fn from_op(op: &str, children: Vec<Id>) -> Result<Self, Self::Error> {
-        Ok(Self::new(op, children))
+        let n = children.len();
+        Ok(Self::new(op, children, vec![(0, 0); n]))
     }
 }
 
@@ -139,14 +121,19 @@ impl FromOp for TileLang {
 /// II, sets up the same ILP (T, K, A, B, span variables; precedence, dep,
 /// buffer, modulo-resource constraints) and minimises span. The first
 /// feasible II is returned.
-pub struct IiExtractor<'a, N: Analysis<TileLang>> {
+pub struct SwpExtractor<'a, N: Analysis<TileLang>, C: CostModel> {
     egraph: &'a EGraph<TileLang, N>,
+    cost_model: C,
     resource_limits: Vec<i32>,
 }
 
-impl<'a, N: Analysis<TileLang>> IiExtractor<'a, N> {
-    pub fn new(egraph: &'a EGraph<TileLang, N>, resource_limits: Vec<i32>) -> Self {
-        Self { egraph, resource_limits }
+impl<'a, N: Analysis<TileLang>, C: CostModel> SwpExtractor<'a, N, C> {
+    pub fn new(
+        egraph: &'a EGraph<TileLang, N>,
+        cost_model: C,
+        resource_limits: Vec<i32>,
+    ) -> Self {
+        Self { egraph, cost_model, resource_limits }
     }
 
     pub fn solve(&self) -> usize {
@@ -167,6 +154,10 @@ impl<'a, N: Analysis<TileLang>> IiExtractor<'a, N> {
             .map(|(i, c)| (c.id, i))
             .collect();
         let n_resources = self.resource_limits.len();
+        let rt: Vec<ResTable> = classes
+            .iter()
+            .map(|c| self.cost_model.get_rt(c.nodes[0].op.as_str()))
+            .collect();
 
         let mut vars = variables!();
         let t: Vec<_> = (0..n)
@@ -186,9 +177,9 @@ impl<'a, N: Analysis<TileLang>> IiExtractor<'a, N> {
         let mut model = vars.minimise(span).using(default_solver);
 
         // 1. Span: t[i] + cost(i) <= span
-        for (i, class) in classes.iter().enumerate() {
-            let node = &class.nodes[0];
-            model.add_constraint(constraint!(t[i] + (node.cost() as f64) <= span));
+        for i in 0..n {
+            let cost = rt[i].len() as f64;
+            model.add_constraint(constraint!(t[i] + cost <= span));
         }
 
         // 2. Each job starts exactly once: sum_t a[t][i] == 1
@@ -235,13 +226,12 @@ impl<'a, N: Analysis<TileLang>> IiExtractor<'a, N> {
         }
 
         // 6. Modulo resource: for each resource s and cycle t in [0, II),
-        //    sum over (i, l) of A[(t-l)%II, i] * restable(i, II)[l][s] <= R[s]
+        //    sum over (i, l) of A[(t-l)%II, i] * modulo_rt, II)[l][s] <= R[s]
         for s in 0..n_resources {
             for tt in 0..ii {
                 let mut expr: Expression = 0.0.into();
-                for (i, class) in classes.iter().enumerate() {
-                    let node = &class.nodes[0];
-                    let table = node.restable(ii, n_resources);
+                for i in 0..n {
+                    let table = modulo_rt(&rt[i], ii, n_resources);
                     for l in 0..ii {
                         let coeff = table[l][s];
                         if coeff == 0 {
@@ -267,15 +257,34 @@ fn main() {
 mod tests {
     use super::*;
 
+    pub struct TestCostModel;
+
+    impl CostModel for TestCostModel {
+        fn get_rt(&self, op: &str) -> ResTable {
+            match op {
+                // a/b/c/d toy problem.
+                "a" => vec![vec![1, 0]],
+                "b" => vec![vec![0, 1]],
+                "c" => vec![vec![1, 0]],
+                "d" => vec![vec![1, 0], vec![0, 1]],
+                // ilp-modulo-sched.py problem. I1=MMA0, I2=SOFTMAX, I3=MMA1.
+                "I1" => vec![vec![1, 0]; 2],
+                "I2" => vec![vec![0, 1]; 4],
+                "I3" => vec![vec![1, 0]; 2],
+                _ => Vec::new(),
+            }
+        }
+    }
+
     #[test]
     fn ii_abcd() {
         let mut egraph: EGraph<TileLang, ()> = EGraph::default();
 
         let d_ph = egraph.add(TileLang::leaf("d"));
-        let c = egraph.add(TileLang::new("c", vec![d_ph]));
-        let b = egraph.add(TileLang::new("b", vec![c, d_ph]));
-        let _a = egraph.add(TileLang::new("a", vec![b]));
-        let d_with_b = egraph.add(TileLang::new("d", vec![b]));
+        let c = egraph.add(TileLang::new("c", vec![d_ph], vec![(0, 1)]));
+        let b = egraph.add(TileLang::new("b", vec![c, d_ph], vec![(0, 1), (0, 2)]));
+        let _a = egraph.add(TileLang::new("a", vec![b], vec![(0, 2)]));
+        let d_with_b = egraph.add(TileLang::new("d", vec![b], vec![(1, 1)]));
         egraph.union(d_ph, d_with_b);
         egraph.rebuild();
 
@@ -289,7 +298,7 @@ mod tests {
             assert_eq!(class.nodes.len(), 1);
         }
 
-        let ii = IiExtractor::new(&egraph, vec![1, 1]).solve();
+        let ii = SwpExtractor::new(&egraph, TestCostModel, vec![1, 1]).solve();
         assert_eq!(ii, 3);
     }
 
@@ -300,9 +309,9 @@ mod tests {
         let mut egraph: EGraph<TileLang, ()> = EGraph::default();
 
         let i1 = egraph.add(TileLang::leaf("I1"));
-        let i2 = egraph.add(TileLang::new("I2", vec![i1]));
+        let i2 = egraph.add(TileLang::new("I2", vec![i1], vec![(2, 0)]));
         let i3_ph = egraph.add(TileLang::leaf("I3"));
-        let i3 = egraph.add(TileLang::new("I3", vec![i2, i3_ph]));
+        let i3 = egraph.add(TileLang::new("I3", vec![i2, i3_ph], vec![(4, 0), (2, 1)]));
         egraph.union(i3_ph, i3);
         egraph.rebuild();
 
@@ -317,7 +326,7 @@ mod tests {
             assert_eq!(class.nodes.len(), 1);
         }
 
-        let ii = IiExtractor::new(&egraph, vec![1, 1]).solve();
+        let ii = SwpExtractor::new(&egraph, TestCostModel, vec![1, 1]).solve();
         assert_eq!(ii, 4);
     }
 }
